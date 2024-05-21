49006eb7da82b423e90f37261067a2fcafbe1ca3
gretaangebrand <ag9716@mci4me.at>
2024-05-21 14:03:23 +0200

    Versuche Skala

--- analyze_activity.py
+++ analyze_activity.py
@@ -13,8 +13,44 @@ def calculate_mean_max(df):
 
 def interactive_plot(df, zones):
     df["Time_in_Seconds"] = df.index
-    fig = px.line(df, x="Time_in_Seconds", y=["PowerOriginal", "HeartRate"], title="Power and Heart Rate over Time")
+    fig = px.line(df, x="Time_in_Seconds", y=["PowerOriginal", "HeartRate"], title="Time in Seconds")
    
+# Konvertiere den Figure in ein `plotly.graph_objects`-Objekt, um zusätzliche Anpassungen vorzunehmen
+    fig = go.Figure(fig)
+
+    fig.add_trace(go.Scatter(x=df["Time_in_Seconds"], y=df["PowerOriginal"],
+                             mode='lines', name='Power (Watts)',
+                             yaxis='y1'))
+
+    # Add Heart Rate trace
+    fig.add_trace(go.Scatter(x=df["Time_in_Seconds"], y=df["HeartRate"],
+                             mode='lines', name='Heart Rate (BPM)',
+                             yaxis='y2'))
+
+   #skala auf der rechtenseit
+    fig.update_layout(
+        title="Power and Heart Rate over Time",
+        xaxis=dict(title='Time (seconds)'),
+        yaxis=dict(title='Power (Watts)', titlefont=dict(color='#1f77b4'),
+                   tickfont=dict(color='#1f77b4')),
+        yaxis2=dict(title='Heart Rate (BPM)', titlefont=dict(color='#ff7f0e'),
+                    tickfont=dict(color='#ff7f0e'),
+                    anchor='x', overlaying='y', side='right')
+    )
+
+    #add zone 1
+    fig.add_shape(type="rect", x0=0, x1=df["Time_in_Seconds"].max(), y0=zones["Zone 1"][0], y1=zones["Zone 1"][1], line=dict(color="red", width=1), fillcolor="red", opacity=0.1, layer="below")
+    #add zone 2
+    fig.add_shape(type="rect", x0=0, x1=df["Time_in_Seconds"].max(), y0=zones["Zone 2"][0], y1=zones["Zone 2"][1], line=dict(color="blue", width=1), fillcolor="blue", opacity=0.1, layer="below")
+    #add zone 3
+    fig.add_shape(type="rect", x0=0, x1=df["Time_in_Seconds"].max(), y0=zones["Zone 3"][0], y1=zones["Zone 3"][1], line=dict(color="green", width=1), fillcolor="green", opacity=0.1, layer="below")
+    #add zone 4
+    fig.add_shape(type="rect", x0=0, x1=df["Time_in_Seconds"].max(), y0=zones["Zone 4"][0], y1=zones["Zone 4"][1], line=dict(color="yellow", width=1), fillcolor="yellow", opacity=0.1, layer="below")
+    #add zone 5
+    fig.add_shape(type="rect", x0=0, x1=df["Time_in_Seconds"].max(), y0=zones["Zone 5"][0], y1=zones["Zone 5"][1], line=dict(color="purple", width=1), fillcolor="purple", opacity=0.1, layer="below")
+    
+    fig.update_layout(legend_title="Legend")
+    
     return fig
 
 def calculate_zones(df, max_hr):
