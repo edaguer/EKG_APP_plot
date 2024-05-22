1b242dc612a786b43d21e2cc1b98663a149fbaaf
edaguer <ge7045@mci4me.at>
2024-05-22 14:32:41 +0200

    Kommentare hinzugefügt und Code verschönert

--- analyze_activity.py
+++ analyze_activity.py
@@ -2,32 +2,33 @@ import pandas as pd
 import plotly.express as px #Leistung über die Zeit in Plot, welcher Leistung und Herzfrequenz über die Zeit anzeigt
 import plotly.graph_objects as go
 
+#Daten aus der CSV-Datei laden
 def load_data():
     df = pd.read_csv("activity.csv")
     return df 
 
+#Berechnungen für Mittelwert & maximale Leistung
 def calculate_mean_max(df):
     mean_power = df["PowerOriginal"].mean()
     max_power = df["PowerOriginal"].max()
     return mean_power, max_power
 
+#interaktiven Plot erstellen
 def interactive_plot(df, zones):
     df["Time_in_Seconds"] = df.index
-    
-   
-# Konvertiere den Figure in ein `plotly.graph_objects`-Objekt, um zusätzliche Anpassungen vorzunehmen
-    fig = go.Figure()
+    fig = go.Figure() #leeren Figure erstellen
 
+    #Leistungsdaten zur Figure hinzufügen
     fig.add_trace(go.Scatter(x=df["Time_in_Seconds"], y=df["PowerOriginal"],
                              mode='lines', name='Power (Watts)',
                              yaxis='y1'))
 
-    # Add Heart Rate trace
+    #Herzfrequenzdaten zur Figure hinzufügen
     fig.add_trace(go.Scatter(x=df["Time_in_Seconds"], y=df["HeartRate"],
                              mode='lines', name='Heart Rate (BPM)',
                              yaxis='y2'))
 
-   #Skala auf beiden Seiten
+   #Layout: Achsen, Legende, Skala auf beiden Seiten
     fig.update_layout(
         title="Power and Heart Rate over Time",
         xaxis=dict(title='Time (seconds)'),
@@ -38,22 +39,22 @@ def interactive_plot(df, zones):
                     anchor='x', overlaying='y', side='right'),
         legend=dict(x = 1.1, y = 1) #Legende weiter rechts positionieren
     )
-
-    #add zone 1
-    fig.add_shape(type="rect", x0=0, x1=df["Time_in_Seconds"].max(), y0=zones["Zone 1"][0], y1=zones["Zone 1"][1], line=dict(color="red", width=1), fillcolor="red", opacity=0.1, layer="below")
-    #add zone 2
-    fig.add_shape(type="rect", x0=0, x1=df["Time_in_Seconds"].max(), y0=zones["Zone 2"][0], y1=zones["Zone 2"][1], line=dict(color="blue", width=1), fillcolor="blue", opacity=0.1, layer="below")
-    #add zone 3
-    fig.add_shape(type="rect", x0=0, x1=df["Time_in_Seconds"].max(), y0=zones["Zone 3"][0], y1=zones["Zone 3"][1], line=dict(color="green", width=1), fillcolor="green", opacity=0.1, layer="below")
-    #add zone 4
-    fig.add_shape(type="rect", x0=0, x1=df["Time_in_Seconds"].max(), y0=zones["Zone 4"][0], y1=zones["Zone 4"][1], line=dict(color="yellow", width=1), fillcolor="yellow", opacity=0.1, layer="below")
-    #add zone 5
-    fig.add_shape(type="rect", x0=0, x1=df["Time_in_Seconds"].max(), y0=zones["Zone 5"][0], y1=zones["Zone 5"][1], line=dict(color="purple", width=1), fillcolor="purple", opacity=0.1, layer="below")
+    #Leistungszonen gefärbt hinzufügen
+    #zone 1
+    fig.add_shape(type="rect", x0=0, x1=df["Time_in_Seconds"].max(), y0=zones["Zone 1"][0], y1=zones["Zone 1"][1], line=dict(color="red", width=1), fillcolor="red", opacity=0.3, layer="below") #opacity für Farbenstärke
+    #zone 2
+    fig.add_shape(type="rect", x0=0, x1=df["Time_in_Seconds"].max(), y0=zones["Zone 2"][0], y1=zones["Zone 2"][1], line=dict(color="blue", width=1), fillcolor="blue", opacity=0.3, layer="below")
+    #zone 3
+    fig.add_shape(type="rect", x0=0, x1=df["Time_in_Seconds"].max(), y0=zones["Zone 3"][0], y1=zones["Zone 3"][1], line=dict(color="green", width=1), fillcolor="green", opacity=0.3, layer="below")
+    #zone 4
+    fig.add_shape(type="rect", x0=0, x1=df["Time_in_Seconds"].max(), y0=zones["Zone 4"][0], y1=zones["Zone 4"][1], line=dict(color="yellow", width=1), fillcolor="yellow", opacity=0.3, layer="below")
+    #zone 5
+    fig.add_shape(type="rect", x0=0, x1=df["Time_in_Seconds"].max(), y0=zones["Zone 5"][0], y1=zones["Zone 5"][1], line=dict(color="purple", width=1), fillcolor="purple", opacity=0.3, layer="below")
     
-    fig.update_layout(legend_title="Legend")
+    fig.update_layout(legend_title="Legend") #Titel für Legende
     
     return fig
-
+#Berechnung der Herzfrequenzzonen basierend auf dem macimalen Herzfrequenzwert
 def calculate_zones(df, max_hr):
     zones = {
         "Zone 1": (0.5 * max_hr, 0.6 * max_hr),
@@ -62,12 +63,13 @@ def calculate_zones(df, max_hr):
         "Zone 4": (0.8 * max_hr, 0.9 * max_hr),
         "Zone 5": (0.9 * max_hr, 1.0 * max_hr)
     }
-    
+    #Herzfrequenzwerte zu den jeweiligen Zonen zuordnen    
     for zone, (lower, upper) in zones.items():
         df[zone] = df["HeartRate"].apply(lambda x: 1 if lower <= x <= upper else 0)
     
     return df, zones
 
+#Berechnung der Zeit in den jeweiligen Zonen
 def calculate_time_in_zones(df, zones):
     time_in_zones = {}
     for zone in zones:
@@ -75,6 +77,7 @@ def calculate_time_in_zones(df, zones):
         time_in_zones[zone] = round(time_in_zone, 2)
     return time_in_zones
 
+#Berechnung der Durchschnittsleistung in den jeweiligen Zonen
 def calculate_avg_power_in_zones(df, zones):
     avg_power_in_zones = {}
     for zone in zones:
